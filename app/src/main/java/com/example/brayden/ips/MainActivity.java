2a8032d64cec79efd90d2492986906fe9bedec0c
BraydenFolk <brayden_folk@hotmail.com>
2017-04-06 10:07:20 -0600

    Add files via upload

--- app/src/main/java/com/example/brayden/ips/MainActivity.java
+++ app/src/main/java/com/example/brayden/ips/MainActivity.java
@@ -18,8 +18,15 @@ import android.content.BroadcastReceiver;
 import android.widget.TextView;
 import android.os.CountDownTimer;
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.Random;
+import android.graphics.Color;
+import android.support.v4.util.ArrayMap;
+import android.support.v7.app.AppCompatActivity;
+import ca.hss.heatmaplib.HeatMap;
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends AppCompatActivity  {
 
     private final static int REQUEST_ENABLE_BT = 1;
     private BluetoothAdapter mBluetoothAdapter;
@@ -44,6 +51,19 @@ public class MainActivity extends ActionBarActivity {
             }
         });
 
+        //Heat map
+
+        HeatMap map = (HeatMap)findViewById(R.id.example_map);
+        map.setMinimum(0.0);
+        map.setMaximum(100.0);
+        Map<Float, Integer> colors = new ArrayMap<>();
+        //build a color gradient in HSV from red at the center to green at the outside
+
+        for (int i = 0; i < 21; i++) {
+            float stop = ((float) i) / 20.0f;
+            int color = doGradient(i * 5, 0, 100, 0xff00ff00, 0xffff0000);
+            colors.put(stop, color);
+        }
         // Register for broadcasts when a device is discovered.
         //IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
         //registerReceiver(mReceiver, filter);
@@ -66,6 +86,36 @@ public class MainActivity extends ActionBarActivity {
         mBluetoothAdapter.startDiscovery();
 
         TimerActivity();
+    }
+
+    private float clamp(float value, float min, float max) {
+        return value * (max - min) + min;
+    }
+
+    private double clamp(double value, double min, double max) {
+        return value * (max - min) + min;
+    }
+    private static int doGradient(double value, double min, double max, int min_color, int max_color) {
+        if (value >= max) {
+            return max_color;
+        }
+        if (value <= min) {
+            return min_color;
+        }
+        float[] hsvmin = new float[3];
+        float[] hsvmax = new float[3];
+        float frac = (float)((value - min) / (max - min));
+        Color.RGBToHSV(Color.red(min_color), Color.green(min_color), Color.blue(min_color), hsvmin);
+        Color.RGBToHSV(Color.red(max_color), Color.green(max_color), Color.blue(max_color), hsvmax);
+        float[] retval = new float[3];
+        for (int i = 0; i < 3; i++) {
+            retval[i] = interpolate(hsvmin[i], hsvmax[i], frac);
+        }
+        return Color.HSVToColor(retval);
+    }
+
+    private static float interpolate(float a, float b, float proportion) {
+        return (a + ((b - a) * proportion));
     }
 
     public void TimerActivity()
@@ -104,8 +154,8 @@ public class MainActivity extends ActionBarActivity {
                         }
                     }
                 }
-                int rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, Short.MIN_VALUE);
-
+                int rssi = ConvertRSSIToReadable(intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, Short.MIN_VALUE));
+                rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, Short.MIN_VALUE);
                 if (found) {
                     arrayOfFoundBTDevices.get(index).setBluetoothRssi(rssi);
                 } else {
@@ -125,6 +175,29 @@ public class MainActivity extends ActionBarActivity {
             }
         }
     };
+
+    private int ConvertRSSIToReadable(int rssi)
+    {
+        int rssiConverted = 0;
+        if (rssi < 0 && rssi > -35)
+        {
+            rssiConverted = 5;
+        }
+        else if (rssi < -35 && rssi > -50)
+        {
+            rssiConverted = 10;
+        }
+        else if (rssi < -50)
+        {
+            rssiConverted = 15;
+        }
+        else
+        {
+            rssiConverted = 20;
+        }
+
+        return rssiConverted;
+    }
 
     public void update()
     {
